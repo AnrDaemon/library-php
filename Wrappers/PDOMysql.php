bd8b37b260583fec899e583e9dab52bbfe09f40a
AnrDaemon <anrdaemon@freemail.ru>
2017-05-30 19:35:31 +0400

    + Dummy MySQL PDO wrapper (preparing for split).

---
+++ Wrappers/PDOMysql.php
@@ -0,0 +1,12 @@
+<?php
+/** Dummy PDOWrapper for MySQL with extensions.
+*
+* @package Wrappers
+* @version SVN: $Id: PDOWrapper.php 634 2017-05-22 13:27:59Z anrdaemon $
+*/
+
+namespace AnrDaemon\Wrappers;
+
+class PDOMysql extends PDOWrapper
+{
+}

34d27e0291fdfd4d9e4beda8c77a5a99fae06e29
AnrDaemon <anrdaemon@yandex.ru>
2017-11-03 03:45:37 +0300

    Moved Net\Url to its rightful place.
    Added more default ports.
    Moved port omission to render.
    Declared virtual class properties.
    Added much more documentation.
    Added legacy Net\Url wrapper.

---
+++ Misc/Url.php
@@ -0,0 +1,18 @@
+<?php
+/** Net\Url legacy wrapper
+*
+* @version SVN: $Id: Url.php 704 2017-11-03 00:39:21Z anrdaemon $
+*/
+
+namespace AnrDaemon\Misc;
+
+trigger_error("Deprecated - please use \\AnrDaemon\\Net\\Url instead", E_USER_DEPRECATED);
+
+/** Net\Url legacy wrapper
+@deprecated 1.7 Deprecated - use {@see \AnrDaemon\Net\Url Net\Url} instead.
+@see \AnrDaemon\Net\Url
+*/
+class Url
+extends \AnrDaemon\Net\Url
+{
+}
--- Net/Url.php
+++ Net/Url.php
@@ -1,21 +1,63 @@
 <?php
 /** URL handling class.
 *
-* @version SVN: $Id: Url.php 697 2017-11-01 17:05:47Z anrdaemon $
+* @version SVN: $Id: Url.php 703 2017-11-03 00:36:44Z anrdaemon $
 */
 
-namespace AnrDaemon\Misc;
+namespace AnrDaemon\Net;
 
 /** A class to simplify handling the various URL's
 *
-* The class is read-only collection, the only way to modify its contents
+* The class is a read-only collection, the only way to modify its contents
 * is to create a new instance of the class.
 *
-* The class is always trying to populate host and scheme
+* The class is always trying to populate host/port and scheme upon creation. You may override
+* them later on using {@see Url::setParts() self::setParts()}.
+*
+* When parsing the input URI or setting parts, empty values are stripped.
+*
+* URL parts can be accessed as properties (`$url->path`), query parts
+* can be accessed as array indices (`$url['param']`).
+*
+* @property-read string $scheme Treatment of some well-known schemes (like http or ldap) is enhanced.
+* @property-read string $user
+* @property-read string $pass
+* @property-read string $host The IDN hosts are decoded.
+* @property-read int $port The port is always converted to integer.
+* @property-read string $path Path always decoded, like `$_SERVER['DOCUMENT_URI']`.
+* @property-read array $query Part after the question mark "?".
+* @property-read string $fragment Part after the hashmark "#".
 */
 class Url
 implements \Iterator, \ArrayAccess, \Countable
 {
+  /**
+  * @var array $defaultPorts The list of well-known schemes and associated ports.
+  * @source 1 16 The current list of well-known schemes:
+  */
+  private static $defaultPorts = array(
+    'ftp' => 21,
+    'ftps' => 990,
+    'gopher' => 70,
+    'http'  => 80,
+    'https' => 443,
+    'imap' => 143,
+    'imaps' => 993,
+    'ldap' => 389,
+    'ldaps' => 636,
+    'nntp' => 119,
+    'nntps' => 563,
+    'pop3' => 110,
+    'pop3s' => 995,
+    'ssh' => 22,
+    'telnet' => 23,
+    'telnets' => 992,
+  );
+
+  /**
+  * @var array $params Internal array holding the URI parts.
+  * @source 1 8 The current list of well-known schemes:
+  */
   protected $params = array(
     'scheme' => null, // - e.g. http
     'user' => null, //
@@ -27,10 +69,10 @@ implements \Iterator, \ArrayAccess, \Countable
     'fragment' => null, // - after the hashmark #
   );
 
-  /** Internal: recursive ksort
+  /** @internal Recursive ksort
   * @param array &$array
   * @return void
-  * @see \ksort
+  * @see \ksort()
   */
   protected function rksort(&$array)
   {
@@ -43,7 +85,7 @@ implements \Iterator, \ArrayAccess, \Countable
 
   /** Parse an URL into replacement parts and create a new class instance using them
   *
-  * Takes apart the $url and uses its parts to call self::setParts().
+  * Takes apart the `$url` and uses its parts to call {@see \AnrDaemon\Net\Url::setParts() self::setParts()}.
   *
   * The user, password and fragment fields are url-decoded.
   *
@@ -52,22 +94,24 @@ implements \Iterator, \ArrayAccess, \Countable
   * The path is url-decoded, except for encoded "/"(%2F) character.
   *
   * The query string is decoded into an array by the extension of using
-  * self::setParts() to compose a new class instance.
+  * {@see \AnrDaemon\Net\Url::setParts() self::setParts()} to compose a new class instance.
   *
   * Note: May not parse all URL's in a desirable way.
   * See f.e. https://3v4l.org/BPsaa for mailto: URI's
   *
-  * @see RFC:3986
-  * @see self::setParts
-  * @see \parse_url
-  * @see \parse_str
+  * @see https://tools.ietf.org/html/rfc3986 [RFC3986]
+  * @see \parse_url()
+  * @see \parse_str()
+  *
+  * @uses \AnrDaemon\Net\Url::setParts() to construct resulting object.
+  *
   * @param string $url An URL to parse.
-  * @return self A new class instance with corresponding parts replaced.
+  * @return Url A new class instance with corresponding parts replaced.
   */
-  function parse($string)
+  function parse($url)
   {
     // TODO: Correctly handle mailto: scheme https://3v4l.org/S0AIa mailto://user@example.org
-    $parts = parse_url($string);
+    $parts = parse_url($url);
     if($parts === false)
       throw new \InvalidArgumentException("Provided string can not be parsed as valid URL.");
 
@@ -91,17 +135,20 @@ implements \Iterator, \ArrayAccess, \Countable
 
   /** Create a new instance of the class by replacing parts in the current instance
   *
-  * Note: This is a replacement, not merge; especially in case of a 'query' part.
+  * Note: This is a replacement, not merge; especially in case of a `query` part.
   *
-  * Note: The 'query' part is always decoded into an array.
+  * Note: The `query` part is always decoded into an array.
   *
   * @param array $parts A set of parts to replace. Uses the same names parse_url uses.
-  * @return self A new class instance with corresponding parts replaced.
+  * @return Url A new class instance with corresponding parts replaced.
   */
   function setParts(array $parts)
   {
-    foreach(array('scheme', 'port', 'user', 'pass', 'path', 'query', 'fragment') as $part)
-      // Avoid creation of array keys if they are not set.
+    /** Reset empty replacement parts to null
+    *
+    * Avoid creation of replacement array keys if they are not set.
+    */
+    foreach(array_keys($this->params) as $part)
       if(isset($parts[$part]) && empty($parts[$part]))
         $parts[$part] = null;
 
@@ -110,13 +157,13 @@ implements \Iterator, \ArrayAccess, \Countable
 
     if(isset($parts['query']))
     {
-      if(!is_array($parts['query']))
+      if(is_array($parts['query']))
       {
-        parse_str($parts['query'], $query);
+        $query = $parts['query'];
       }
       else
       {
-        $query = $parts['query'];
+        parse_str($parts['query'], $query);
       }
 
       $this->rksort($query);
@@ -130,21 +177,29 @@ implements \Iterator, \ArrayAccess, \Countable
 
 // ArrayAccess
 
+  /** @internal
+  @see \ArrayAccess::offsetExists() */
   public function offsetExists($offset)
   {
     return isset($this->params['query'][$offset]);
   }
 
+  /** @internal
+  @see \ArrayAccess::offsetGet() */
   public function offsetGet($offset)
   {
     return $this->params['query'][$offset];
   }
 
+  /** @internal
+  @see \ArrayAccess::offsetSet() */
   public function offsetSet($offset, $value)
   {
     throw new \LogicException('Forbidden.');
   }
 
+  /** @internal
+  @see \ArrayAccess::offsetUnset() */
   public function offsetUnset($offset)
   {
     throw new \LogicException('Forbidden.');
@@ -152,6 +207,8 @@ implements \Iterator, \ArrayAccess, \Countable
 
 // Countable
 
+  /** @internal
+  @see \Countable::count() */
   public function count()
   {
     return empty($this->params['query']) ? 0 : count($this->params['query']);
@@ -159,6 +216,8 @@ implements \Iterator, \ArrayAccess, \Countable
 
 // Iterator
 
+  /** @internal
+  @see \Iterator::current() */
   public function current()
   {
     if(is_array($this->params['query']))
@@ -167,6 +226,8 @@ implements \Iterator, \ArrayAccess, \Countable
       return null;
   }
 
+  /** @internal
+  @see \Iterator::key() */
   public function key()
   {
     if(is_array($this->params['query']))
@@ -175,18 +236,24 @@ implements \Iterator, \ArrayAccess, \Countable
       return null;
   }
 
+  /** @internal
+  @see \Iterator::next() */
   public function next()
   {
     if(is_array($this->params['query']))
       next($this->params['query']);
   }
 
+  /** @internal
+  @see \Iterator::rewind() */
   public function rewind()
   {
     if(is_array($this->params['query']))
       reset($this->params['query']);
   }
 
+  /** @internal
+  @see \Iterator::valid() */
   public function valid()
   {
     return !is_null($this->key());
@@ -198,15 +265,14 @@ implements \Iterator, \ArrayAccess, \Countable
   *
   * Try hard to discover the request scheme, server name and port.
   *
-  * The server name is looked in $_SERVER['SERVER_NAME'], then in
-  * $_SERVER['HTTP_HOST'], if not found.
+  * The server name is looked in `$_SERVER['SERVER_NAME']`, then in
+  * `$_SERVER['HTTP_HOST']`, if not found.
   *
-  * The server port is taken from $_SERVER['SERVER_PORT'], or if
-  * $_SERVER['HTTP_HOST'] is used to set the server name, the port is looked in there as well.
+  * The server port is taken from `$_SERVER['SERVER_PORT']`, or if
+  * `$_SERVER['HTTP_HOST']` is used to set the server name, the port
+  * is looked in there as well.
   *
-  * If request scheme is http or https, the default port is then stripped from the input.
-  *
-  * Hint: Provide an empty $query array to override any potential $baseUrl query part.
+  * Hint: Provide an empty `$query` array to override any potential `$baseUrl` query part.
   *
   * @param ?string $baseUrl An optional initial URL to set defaults from.
   * @param ?array $query An optional query key-value pairs.
@@ -241,14 +307,6 @@ implements \Iterator, \ArrayAccess, \Countable
       }
     }
 
-    if(
-      ($parts['scheme'] === 'http' && $parts['port'] == 80) ||
-      ($parts['scheme'] === 'https' && $parts['port'] == 443)
-    )
-    {
-      $parts['port'] = null;
-    }
-
     if(isset($query))
     {
       $parts['query'] = $query;
@@ -257,16 +315,25 @@ implements \Iterator, \ArrayAccess, \Countable
     $this->params = $this->setParts($parts)->params;
   }
 
+  /** @internal */
   function __get($index)
   {
     return $this->params[$index];
   }
 
+  /** @internal */
   function __isset($index)
   {
     return isset($this->params[$index]);
   }
 
+  /** Converts URL to a sting representation.
+  *
+  * If URI scheme is specified, some well-known schemes are considered
+  * and default port number is omitted from the resulting URI.
+  *
+  * @return string an URL-encoded string representation of the object.
+  */
   function __toString()
   {
     $parts = $this->params;
@@ -290,6 +357,10 @@ implements \Iterator, \ArrayAccess, \Countable
       }
 
       $result .= idn_to_ascii($parts['host']);
+
+      if(isset($parts['port'], $parts['scheme'], $this::$defaultPorts[$parts['scheme']]))
+        if($parts['port'] == $this::$defaultPorts[$parts['scheme']])
+          unset($parts['port']);
 
       if(isset($parts['port']))
         $result .= ":" . $parts['port'];

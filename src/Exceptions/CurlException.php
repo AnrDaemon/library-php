9bd0df32b937d7b1e5679a385c8f5ed89a286789
AnrDaemon <anrdaemon@yandex.ru>
2019-03-10 23:48:18 +0300

    Improved CurlException error message construction.
    Difference in curl_error vs. curl_strerror was a nasty surprize.

--- src/Exceptions/CurlException.php
+++ src/Exceptions/CurlException.php
@@ -1,7 +1,7 @@
 <?php
 /** Curl errors wrapper
 *
-* @version SVN: $Id: CurlException.php 788 2018-04-03 14:58:56Z anrdaemon $
+* @version SVN: $Id: CurlException.php 993 2019-03-04 18:12:58Z anrdaemon $
 */
 
 namespace AnrDaemon\Exceptions;
@@ -11,10 +11,22 @@ extends \RuntimeException
 {
   public function __construct($curl = null, \Exception $previous = null)
   {
-    parent::__construct(
-      is_resource($curl) ? curl_error($curl) : ($curl ?: 'Unable to initialize cURL instance - unknown error.'),
-      is_resource($curl) ? curl_errno($curl) : 0,
-      $previous
-    );
+    if(is_resource($curl))
+    {
+      $error = curl_errno($curl);
+      $message = version_compare(PHP_VERSION, '5.5.0', '<') ? "cURL error #{$error}" : curl_strerror($error);
+      $text = curl_error($curl);
+      if(!empty($text))
+      {
+        $message .= ": {$text}";
+      }
+    }
+    else
+    {
+      $message = $curl ?: 'Unable to initialize cURL instance: unknown error';
+      $error = 0;
+    }
+
+    parent::__construct($message, $error, $previous);
   }
 }

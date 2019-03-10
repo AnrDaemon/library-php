71c03b0a527912b3a851696ffb05cc711bfa028b
AnrDaemon <anrdaemon@yandex.ru>
2019-03-10 23:40:52 +0300

    Simplified classloader a little bit.
    Yes, again.

--- src/classloader.php
+++ src/classloader.php
@@ -1,29 +1,32 @@
 <?php
 /** Universal stackable classloader.
 *
-* @version SVN: $Id: classloader.php 740 2018-03-03 23:50:24Z anrdaemon $
+* @version SVN: $Id: classloader.php 1001 2019-03-10 20:40:08Z anrdaemon $
 */
 
 namespace AnrDaemon;
 
-return \call_user_func(function(){
-  $nsl = \strlen(__NAMESPACE__);
-  return \spl_autoload_register(
-    function($className)
-    use($nsl)
-    {
-      if(\strncmp($className, __NAMESPACE__, $nsl) !== 0)
-        return;
-
-      $className = \substr($className, $nsl);
-      if(\strlen($className) < 2)
-        return;
-
-      $path = \realpath(__DIR__ . \strtr("$className.php", '\\', '/'));
-      if(!empty($path))
+return \call_user_func(
+  function()
+  {
+    $nsl = \strlen(__NAMESPACE__);
+    return \spl_autoload_register(
+      function($className)
+      use($nsl)
       {
-        return include_once $path;
+        if(\strncmp($className, __NAMESPACE__, $nsl) !== 0)
+          return;
+
+        $className = \substr($className, $nsl);
+        if($className[0] !== "\\")
+          return;
+
+        $path = __DIR__ . \strtr("$className.php", '\\', '/');
+        if(\file_exists($path))
+        {
+          return include $path;
+        }
       }
-    }
-  );
-});
+    );
+  }
+);
